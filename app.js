18227444ef322dd1d21df5d2364a9eb1057b6153
VraoNOVA <vivek.rao.vivek@gmail.com>
2022-06-25 12:42:05 -0400

    Update app.js
    updated app js to handle events and lets uers talk to each other

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var k = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(k.static(__dirname));
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');});
